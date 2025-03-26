fd8f7cb79e8cb4c5de8945f16df4dde27bfec307
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-03-26 19:52:26 +0900

    トップページの編集、新規投稿・投稿一覧の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+
+    def index
+        @posts = Post.all.order(updated_at: :desc)
+    end
+
+    def new
+        @post = Post.new #空のインスタンスを生成
+    end
+
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to action: 'index' 
+        else
+            redirect_to action: 'new' 
+        end
+    end
+
+    private
+    def post_params #ストロングパラメータ
+        params.require(:post).permit(:title, :content) #パラメーターのキー
+    end
 end
