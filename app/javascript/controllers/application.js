7466b25d05db40d8a25eab92fa595191637578bc
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-03-29 16:26:33 +0900

    元に戻した

--- app/javascript/controllers/application.js
+++
@@ -1,9 +0,0 @@
-import { Application } from "stimulus";
-import { definitionsFromContext } from "stimulus/webpack-helpers";
-
-// コントローラの設定
-const application = Application.start();
-
-// コントローラの定義をロード
-const context = require.context("controllers", true, /.js$/);
-application.load(definitionsFromContext(context));
