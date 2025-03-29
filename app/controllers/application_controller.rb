b282e8dcaa06f585d5e25c3beedf0d98ef68c03d
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-03-29 16:56:30 +0900

    falsh[:notice]の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :ensure_html_format
   
   def after_sign_in_path_for(resource)
-    flash[:notice] = "正常にログインしました。" # 
+    flash[:notice] = "正常にログインしました。" 
     posts_path
   end
 
