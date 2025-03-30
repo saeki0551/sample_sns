105abc58ed4f8e232f41e3fc12fd0f9b6ad73e7c
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-03-30 10:11:49 +0900

    元に戻した

--- config/puma.rb
+++ config/puma.rb
@@ -40,4 +40,3 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
 
-bind "tcp://0.0.0.0:3000"
