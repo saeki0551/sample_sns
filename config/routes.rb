1d32deaf097bced1d8846fc1c50bae3ba11a189d
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-04-02 16:28:56 +0900

    リンクの管轄？の更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  devise_scope :user do
+    get "users/sign_out", to: "devise/sessions#destroy" 
+  end
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -12,9 +17,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#top"
-
-  get "users/sign_out", to: "devise/sessions#destroy" 
-
   resources :posts
 
   get "/posts/:id/destroy", to: "posts#destroy"
