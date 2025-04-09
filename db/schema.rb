1624c34254388eff10f8e735377b1bd69f8a6117
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-04-09 09:35:42 +0900

    Configure database and apply migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_03_26_025149) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
